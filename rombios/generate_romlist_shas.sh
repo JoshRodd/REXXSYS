838b511abafbaf085a68d51a9d5c6b6759e6c92a
Josh Rodd <josh@rodd.us>
2020-09-28 14:13:49 -0400

    Move rombios/ to @JoshRodd:bios8530

--- rombios/generate_romlist_shas.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(cd build; openssl dgst -sha256 *.BIN *.BAD) > romlist_sha.txt
--- rombios/output.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-make >/dev/null 2>&1
-./print_diff.sh|bin/romcompare|sed s';\[/b\]\[/u\]\[u\]\[b\];;'g|pbcopy
