bd9b88163d196decfcaff13c28068297d52946ae
Josh Rodd <jrodd@varmour.com>
2020-10-13 10:32:04 -0400

    REXXSYS.ASM now builds REXXSYS.SYS

--- rexxsys/build.sh
+++ rexxsys/build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-export IFILE=rexxsys.asm; sed -i \~ 's/^ *.model flat$//' rexxsys.asm && uasm -0 -bin -Fo=rexxsys.bin rexxsys.asm
+export IFILE=rexxsys.asm
+
+#sed -i \~ 's/^ *.model flat$//' rexxsys.asm && uasm -0 -Sa -Sg -Sn -bin -Fo=rexxsys.bin -Fl=rexxsys.lst rexxsys.asm || exit
+uasm -0 -Sa -Sg -Sn -bin -Fo=rexxsys.bin -Fl=rexxsys.lst rexxsys.asm || exit
+
+if [ ! -f rexxsys.sys ]; then
+    cp archives/rexxsys.sys .
+fi
+
+if cmp rexxsys.bin rexxsys.sys; then
+    rm -f rexxsys.bin rexxsys.sys
+    printf "REXXSYS.ASM matches REXXSYS.SYS\n"
+else
+    diff <(xxd rexxsys.sys) <(xxd rexxsys.bin) | less
+fi
